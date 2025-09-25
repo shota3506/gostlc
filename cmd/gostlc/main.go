a6dc9d7d8705ece031c411dab404c6bc5449ff39
shota3506 <s.shota.710.3506@gmail.com>
2025-09-25 17:33:13 +0900

    implement repl interpreter

--- cmd/gostlc/main.go
+++ cmd/gostlc/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"github.com/shota3506/gostlc/internal/eval"
 	"github.com/shota3506/gostlc/internal/parser"
@@ -76,6 +78,25 @@ func isInteractive() bool {
 	return (fi.Mode() & os.ModeCharDevice) != 0
 }
 
+func evaluate(code string) (eval.Value, error) {
+	expr, err := parser.Parse(code)
+	if err != nil {
+		return nil, err
+	}
+
+	typedExpr, err := types.Check(expr)
+	if err != nil {
+		return nil, err
+	}
+
+	value, err := eval.Eval(typedExpr)
+	if err != nil {
+		return nil, err
+	}
+
+	return value, nil
+}
+
 func runFile(filename string) error {
 	data, err := os.ReadFile(filename)
 	if err != nil {
@@ -93,17 +114,7 @@ func runStdin() error {
 }
 
 func runCode(code string) error {
-	expr, err := parser.Parse(code)
-	if err != nil {
-		return err
-	}
-
-	typedExpr, err := types.Check(expr)
-	if err != nil {
-		return err
-	}
-
-	resp, err := eval.Eval(typedExpr)
+	resp, err := evaluate(code)
 	if err != nil {
 		return err
 	}
@@ -113,8 +124,76 @@ func runCode(code string) error {
 }
 
 func startREPL() error {
-	fmt.Println("STLC REPL (type :quit to exit)")
-	fmt.Println("REPL not yet implemented")
-	// TODO: Implement REPL
+	fmt.Println("STLC REPL")
+	fmt.Println("Type :quit or :q to exit, :help for help")
+	fmt.Println()
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("gostlc> ")
+
+		if !scanner.Scan() {
+			fmt.Println()
+			break
+		}
+
+		line := strings.TrimSpace(scanner.Text())
+
+		if line == "" {
+			continue
+		}
+
+		if strings.HasPrefix(line, ":") {
+			if err := handleCommand(line); err != nil {
+				if err.Error() == "quit" {
+					fmt.Println("Goodbye!")
+					return nil
+				}
+				fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			}
+			continue
+		}
+
+		if err := evalAndPrint(line); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("error: %w", err)
+	}
+
+	fmt.Println("Goodbye!")
+	return nil
+}
+
+func handleCommand(cmd string) error {
+	switch cmd {
+	case ":quit", ":q":
+		return errors.New("quit")
+	case ":help", ":h":
+		fmt.Println("REPL Commands:")
+		fmt.Println("  :quit, :q  - Exit the REPL")
+		fmt.Println("  :help, :h  - Show this help message")
+		fmt.Println()
+		fmt.Println("Examples:")
+		fmt.Println("  42")
+		fmt.Println("  true")
+		fmt.Println("  (\\x:Int.x) 42")
+		fmt.Println("  (\\f:Int->Int.\\x:Int.f (f x))")
+		return nil
+	default:
+		return fmt.Errorf("unknown command: %s", cmd)
+	}
+}
+
+func evalAndPrint(code string) error {
+	resp, err := evaluate(code)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("=> %s\n", resp)
 	return nil
 }
